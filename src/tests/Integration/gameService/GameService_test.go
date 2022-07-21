9e1d18d44747cf018ec58b0517d40be439e572eb
devMarcus21 <marcusaleclee2000@gmail.com>
2022-07-21 13:47:58 -0700

    Added tests for GameService as well as some edits. Need more testing

---
+++ src/tests/Integration/gameService/GameService_test.go
@@ -0,0 +1,30 @@
+package gameServiceTests
+
+import (
+	"fmt"
+	"testing"
+
+	//"github.com/devMarcus21/GoBombsweeper/src/game"
+	"github.com/devMarcus21/GoBombsweeper/src/gameService"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShouldCreateNewGameService(t *testing.T) {
+	service := gameService.CreateGameService()
+
+	assert.NotNil(t, service, "Should create GameService instance")
+}
+
+func TestShouldCreateANewGame(t *testing.T) {
+	service := gameService.CreateGameService()
+	expectedRow := 8
+	expectedCol := 8
+	expectedBombCount := 2
+
+	err, id := service.CreateNewGoBombsweeperGame(expectedRow, expectedCol, expectedBombCount)
+
+	fmt.Println("game id: "+id)
+
+	assert.Nil(t, err, "Should not return errors")
+	assert.NotEqual(t, "", id, "Should not be empty string")
+}
